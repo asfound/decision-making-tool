7cd6134f2d920b4b3678f509be3c0b490a3066d6
Ksenia Gorina <fffound27@gmail.com>
2025-03-13 00:18:53 +0100

    feat: finish start page layout and styles

--- decision-making-tool/src/view/start-screen/start-screen-view.ts
+++ decision-making-tool/src/view/start-screen/start-screen-view.ts
@@ -4,9 +4,10 @@ import { OptionsListController } from '~/components/options-list/options-list-co
 import { OptionsListModel } from '~/components/options-list/options-list-model';
 import { OptionsList } from '~/components/options-list/options-list-view/options-list-view';
 import { Textarea } from '~/components/textarea/textarea';
-import { section } from '~/utils/create-element';
+import { div, section } from '~/utils/create-element';
 import { View } from '~/view/view';
 
+import styles from './start-screen.module.css';
 export default class StartScreenView extends View<'section'> {
   protected view: HTMLElement;
   private readonly optionsListModel: OptionsListModel;
@@ -26,7 +27,9 @@ export default class StartScreenView extends View<'section'> {
   }
 
   protected createHTML(): HTMLElement {
-    const sectionElement = section({}, ['StartScreen']);
+    const sectionElement = section({ className: styles.section });
+
+    const buttonsContainer = div({ className: styles.container });
 
     const addOptionButton = this.createAddOptionButton();
 
@@ -38,19 +41,24 @@ export default class StartScreenView extends View<'section'> {
 
     const pasteListButton = this.createPasteListButton();
 
-    sectionElement.append(
-      this.optionsList.getHTML(),
+    const startButton = this.createStartButton();
+
+    buttonsContainer.append(
       addOptionButton,
       clearListButton,
+      pasteListButton,
       saveListButton,
       loadListButton,
-      pasteListButton
+      startButton
     );
+
+    sectionElement.append(this.optionsList.getHTML(), buttonsContainer);
     return sectionElement;
   }
 
   private createAddOptionButton(): HTMLButtonElement {
     const addOptionButton = new Button({
+      className: styles.button,
       textContent: 'add option',
       type: 'button',
       onClick: (): void => {
@@ -63,6 +71,7 @@ export default class StartScreenView extends View<'section'> {
 
   private createClearListButton(): HTMLButtonElement {
     const clearListButton = new Button({
+      className: styles.button,
       textContent: 'clear list',
       type: 'button',
       onClick: (): void => {
@@ -75,6 +84,7 @@ export default class StartScreenView extends View<'section'> {
 
   private createSaveListButton(): HTMLButtonElement {
     const saveListButton = new Button({
+      className: styles.button,
       textContent: 'save list to file',
       type: 'button',
       onClick: (): void => {
@@ -87,6 +97,7 @@ export default class StartScreenView extends View<'section'> {
 
   private createLoadListButton(): HTMLButtonElement {
     const loadListButton = new Button({
+      className: styles.button,
       textContent: 'load list from file',
       type: 'button',
       onClick: (): void => {
@@ -99,7 +110,8 @@ export default class StartScreenView extends View<'section'> {
 
   private createPasteListButton(): HTMLButtonElement {
     const pasteListButton = new Button({
-      textContent: 'paste list from file',
+      className: styles.button,
+      textContent: 'paste list',
       type: 'button',
       onClick: (): void => {
         const textareaElement = new Textarea();
@@ -113,5 +125,18 @@ export default class StartScreenView extends View<'section'> {
     });
 
     return pasteListButton.getHTML();
+  }
+
+  private createStartButton(): HTMLButtonElement {
+    const startButton = new Button({
+      className: styles.button,
+      textContent: 'start',
+      type: 'button',
+      onClick: (): void => {
+        console.log(startButton);
+      },
+    });
+
+    return startButton.getHTML();
   }
 }
