43c4a2312343249c6422e964a0205c8974c90535
Ksenia Gorina <fffound27@gmail.com>
2025-03-17 23:51:36 +0100

    refactor: clean up type guards

--- decision-making-tool/src/utils/type-guards.ts
+++ decision-making-tool/src/utils/type-guards.ts
@@ -6,20 +6,18 @@ export function isNonNullable<T>(value: T): value is NonNullable<T> {
   return value !== null && value !== undefined;
 }
 
-export function isListData(data: unknown): asserts data is ListData {
-  if (!isNonNullable(data)) {
+export function assertNotNull<T>(
+  value: T | null | undefined
+): asserts value is NonNullable<T> {
+  if (!isNonNullable(value)) {
     throw new Error(ERRORS.NULLABLE_VALUE_ERROR);
-  }
-
-  if (typeof data !== 'object' || !('list' in data) || !('lastId' in data)) {
-    throw new Error(ERRORS.INVALID_TYPE_ERROR);
   }
 }
 
-export function assertNotNull<T>(
-  value: T | null | undefined
-): asserts value is T {
-  if (value === null || value === undefined) {
-    throw new Error(ERRORS.NULLABLE_VALUE_ERROR);
+export function isListData(data: unknown): asserts data is ListData {
+  assertNotNull(data);
+
+  if (typeof data !== 'object' || !('list' in data) || !('lastId' in data)) {
+    throw new Error(ERRORS.INVALID_TYPE_ERROR);
   }
 }
